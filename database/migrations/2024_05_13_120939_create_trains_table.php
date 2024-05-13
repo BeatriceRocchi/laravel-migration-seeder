03a72e2bac0ee88d80b07131f6a42efb32b940ee
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-05-13 14:34:59 +0200

    [add] table "trains" and its migration

---
+++ database/migrations/2024_05_13_120939_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 25);
+            $table->string('code', 10);
+            $table->string('slug', 50)->unique();
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->tinyInteger('wagons')->unsigned();
+            $table->boolean('is_on_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,7 @@
     {{-- Styles --}}
     @vite('resources/js/app.js')
 
-    <title>Template</title>
+    <title>Train Station</title>
 </head>
 
 <body>
